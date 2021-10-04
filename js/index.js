d3aa6434e448afb5b605957476dd65bdc11e815c
blessedjasonmwanza <mwanzabj@gmail.com>
2021-10-04 17:36:05 +0200

    Fix CSS linter errors and es linter errors

--- js/index.js
+++ js/index.js
@@ -1,29 +1,29 @@
-document.addEventListener('DOMContentLoaded', function(e){
-    let navTrigger = document.querySelector('.mobile-nav-trigger');
-    let nav = document.querySelector('.desktop-nav-menu');
-    let closeNavBtn = document.querySelector('.close-nav');
-  
-    navTrigger.addEventListener('click', function(e){
-      toggleOnMobile(nav);
-    });
-    closeNavBtn.addEventListener('click', function(e){
-      toggleOnMobile(nav);
-    });
-    nav.querySelectorAll('a').forEach(menuItem => {
-      menuItem.addEventListener('click', function(e){
-        toggleOnMobile(nav);
-      });
-    });
-  });
-  
-  function toggleOnMobile(elem){
-    if(window.innerWidth <= 992){
-      let elemVisible = (elem.style.display.length > 0 && elem.style.display !== "none") ? true : false;
-      if(elemVisible){
-        elem.style.display = "none";
-      }else{
-        elem.style.display = "flex";
-      }
+function toggleOnMobile(elem) {
+  if (window.innerWidth <= 992) {
+    const elemVisible = (elem.style.display.length > 0 && elem.style.display !== 'none');
+    if (elemVisible) {
+      elem.style.display = 'none';
+    } else {
+      elem.style.display = 'flex';
     }
   }
-  
+}
+document.addEventListener('DOMContentLoaded', () => {
+  const navTrigger = document.querySelector('.mobile-nav-trigger');
+  const nav = document.querySelector('.desktop-nav-menu');
+  const closeNavBtn = document.querySelector('.close-nav');
+
+  navTrigger.addEventListener('click', () => {
+    toggleOnMobile(nav);
+  });
+
+  closeNavBtn.addEventListener('click', () => {
+    toggleOnMobile(nav);
+  });
+
+  nav.querySelectorAll('a').forEach((menuItem) => {
+    menuItem.addEventListener('click', () => {
+      toggleOnMobile(nav);
+    });
+  });
+});
